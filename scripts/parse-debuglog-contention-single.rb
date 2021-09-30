f977dbce7a226d851dd5796792bf8eaa4fbb58d2
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2021-09-30 08:19:41 +0200

    update regex

--- scripts/parse-debuglog-contention-single.rb
+++ scripts/parse-debuglog-contention-single.rb
@@ -7,7 +7,7 @@ require "pp"
 # 2021-09-07T14:47:21Z [opencon] Enter: lock contention cs_main, net_processing.cpp:1152 completed (18258μs)
 def parse(filename)
     File.read(filename).each_line do |l|
-        m = /\[(.*)\].* lock contention (.*) completed \((\d*).?s\)/.match(l)
+        m = /\[([^\]]*).* lock contention (.*) completed \((\d*).?s\)/.match(l)
         next unless m
         thread = m[1]
         where = m[2]
