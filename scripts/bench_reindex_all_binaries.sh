5105abccdc8d5cc63da960d10311cb1cba338035
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2021-10-03 09:02:07 +0200

    better bench script, now with sudo and clearing caches

--- scripts/bench_reindex_all_binaries.sh
+++ scripts/bench_reindex_all_binaries.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 
-echo "usage: $0 <bitcoind1> <bitcoind2> <bitcoind3> ..."
+echo "usage: sudo $0 <bitcoind1> <bitcoind2> <bitcoind3> ..."
 
 DATADIR=/home/martinus/bitcoin/db/
 
@@ -10,11 +10,11 @@ DBCACHE=4000
 
 # block 690000, see https://www.blockchain.com/btc/block/690000
 ASSUMEVALID=00000000000000000002a23d6df20eecec15b21d32c75833cce28f113de888b7
-ASSUMEVALID=0
-STOPATHEIGHT=300000
+#ASSUMEVALID=0
+STOPATHEIGHT=400000
 #STOPATHEIGHT=690000
 
-sudo pyperf system tune
+pyperf system tune
 
 for BITCOIND in $@
 do
@@ -22,7 +22,14 @@ do
     OUTPREFIX=$(basename ${BITCOIND})
 
     rm -f ${PWD}/${OUTPREFIX}.debug.log
-    /usr/bin/time -v ${BITCOIND} -datadir=${DATADIR} -dbcache=${DBCACHE} -assumevalid=${ASSUMEVALID} -reindex-chainstate -printtoconsole=0 -stopatheight=${STOPATHEIGHT} -debuglogfile=${PWD}/${OUTPREFIX}.debug.log >${OUTPREFIX}.time.txt 2>&1
+
+    sync
+
+    # Clear pagecache, dentries, and inodes. See https://www.tecmint.com/clear-ram-memory-cache-buffer-and-swap-space-on-linux/
+    echo 3 >/proc/sys/vm/drop_caches
+
+    # su ${SUDO_USER} -c "/usr/bin/time -v ${BITCOIND} -datadir=${DATADIR} -dbcache=${DBCACHE} -assumevalid=${ASSUMEVALID} -reindex-chainstate -printtoconsole=0 -stopatheight=${STOPATHEIGHT} -debuglogfile=${PWD}/${OUTPREFIX}.debug.log >${OUTPREFIX}.time.txt 2>&1"
+    su ${SUDO_USER} -c "perf stat ${BITCOIND} -datadir=${DATADIR} -dbcache=${DBCACHE} -assumevalid=${ASSUMEVALID} -reindex-chainstate -printtoconsole=0 -stopatheight=${STOPATHEIGHT} -debuglogfile=${PWD}/${OUTPREFIX}.debug.log >${OUTPREFIX}.stats.txt 2>&1"
 done
 
-sudo pyperf system reset
+pyperf system reset
